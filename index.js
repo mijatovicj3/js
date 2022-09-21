dd0025080ac9f37b66b115ac4d6073c20f144caa
mijatovicj3 <98957665+mijatovicj3@users.noreply.github.com>
2022-09-21 10:42:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let x = 7;
 
 // gornji dio piramide
 for (let i = 1; i <= x; i+=2) {
-  // printing spaces
+
   for (let j = 1; j <= x - i/2; j++) {
     process.stdout.write(' ')
   }
-  // printing star
+  
   for (let k = 0; k <  i ; k++) {
     process.stdout.write('*')
   }
